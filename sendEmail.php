518993198b704608650424cfa6046638995f5efe
Guillermo Garcia Saldierna <ggarciasaldierna@nevada.unr.edu>
2019-06-20 09:57:55 -0700

    Add files via upload

--- sendEmail.php
+++ sendEmail.php
@@ -8,9 +8,6 @@
   $email_subject = "New Form submission";
   $email_body = "You have received a new message from the user $name.\n".
                             "Here is the message:\n $message".
-  $to = "ggarciasaldierna@nevada.unr.edu";
-  $headers = "From: $email_from \r\n";
-  $headers .= "Reply-To: $visitor_email \r\n";
-  mail($to,$email_subject,$email_body,$headers);
+  
 
 ?>
